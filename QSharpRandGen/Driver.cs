6d79e3335677735102dfa8bb0e3e8b8d43f184f4
IAmAPersson <iamaperson620@gmail.com>
2020-01-24 00:53:00 -0600

    is now functional

--- QSharpRandGen/Driver.cs
+++ QSharpRandGen/Driver.cs
@@ -1,17 +1,45 @@
 ﻿using System;
+using System.CodeDom.Compiler;
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.Quantum.Simulation.Core;
 using Microsoft.Quantum.Simulation.Simulators;
+using Quantum.QSharpRandGen;
 
-namespace Quantum.QSharpRandGen
+
+[SuppressMessage("ReSharper", "IdentifierTypo")]
+// ReSharper disable once CheckNamespace
+class Driver
 {
-    class Driver
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            using (var qsim = new QuantumSimulator())
-            {
-                HelloQ.Run(qsim).Wait();
-            }
-        }
+        QuantumRandom r = new QuantumRandom();
+
+        for (int i = 0; i < 5; i++)
+            Console.WriteLine(r.NextInt());
+
+        Console.ReadKey();
+    }
+}
+
+[SuppressMessage("ReSharper", "IdentifierTypo")]
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+class QuantumRandom
+{
+    private readonly QuantumSimulator qsim;
+    public QuantumRandom()
+    {
+        this.qsim = new QuantumSimulator();
+    }
+
+    public int NextInt()
+    {
+        IQArray<Result> results = Generate.Run(qsim).Result;
+        int result = 0;
+
+        for (int i = 0; i < 32; i++)
+            if (results[i] == Result.One)
+                result |= 1 << i;
+
+        return result;
     }
 }
--- QSharpRandGen/Operations.qs
+++ QSharpRandGen/Operations.qs
@@ -3,7 +3,31 @@
     open Microsoft.Quantum.Intrinsic;
     open Microsoft.Quantum.Canon;
     
-    operation HelloQ () : Unit {
-        Message("Hello quantum world!");
+    operation Set(bits : Qubit[], desired : Result) : Unit {
+        for (i in bits) {
+            if (desired != M(i)) {
+                X(i);
+            }
+        }
+    }
+
+    operation Generate () : Result[] {
+        mutable res = new Result[32];
+
+        using (bits = Qubit[8]) {
+            for (i in 0 .. 3) {
+                for (j in 0 .. 7) {
+                    H(bits[j]);
+                }
+
+                for (j in 0 .. 7) {
+                    set res w/= i * 8 + j <-  M(bits[j]);
+                }
+                
+                Set(bits, Zero);
+            }
+
+            return res;
+        }
     }
 }
